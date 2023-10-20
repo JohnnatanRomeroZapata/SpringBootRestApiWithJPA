9c3485ce5fd5625f477940f7f87e9b1e8f2e243a
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-20 19:50:03 -0400

    project init

---
+++ src/test/java/com/example/SpringBootRestApiWithJPA/SpringBootRestApiWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootRestApiWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
